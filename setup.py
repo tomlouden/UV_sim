593b8cebd650e2113ee5efbf421bda71121bd110
Tom Louden <t.m.louden@warwick.ac.uk>
2018-12-17 15:21:22 +0000

    restruct

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup, Extension
+import numpy.distutils.misc_util
+import numpy as np
+import os
+
+command = 'git log -1 | grep ^commit | cut -d " " -f 2 > version_hash'
+
+x = os.system(command)
+
+setup(	name='UV_sim', 
+	version="0.1",
+	author='Tom Louden',
+	author_email = 't.louden@warwick.ac.uk',
+	url = 'https://github.com/tomlouden/UV_sim',
+	packages =['uv_sim'],
+	license = ['GNU GPLv3'],
+	description ='uv sim',
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering',
+		'Programming Language :: Python'
+		],
+	include_dirs = [np.get_include()],
+	install_requires = ['numpy'],
+)
